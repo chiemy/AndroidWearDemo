fef443efb4b6d8457929bf269a202beb0ed06a1e
chiemy <luobaolong@gmail.com>
2015-11-16 14:46:40 +0800

    CardFrame

---
+++ wear/src/main/java/com/chiemy/androidweardemo/activity/CardFrameActivity.java
@@ -0,0 +1,17 @@
+package com.chiemy.androidweardemo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.chiemy.androidweardemo.R;
+
+/**
+ * Created by chiemy on 15/11/16.
+ */
+public class CardFrameActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_frame);
+    }
+}
